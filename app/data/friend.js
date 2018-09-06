49c8482432c6a41b58120d2a6e39cd377bde6a6c
Scott Saloom <scottsaloom@gmail.com>
2018-09-06 16:03:17 -0500

    Updated Code

---
+++ app/data/friend.js
@@ -0,0 +1,82 @@
+var friends = [
+    {
+        name: "Wade Wilson",
+        photo: "https://cdn-images-1.medium.com/max/1920/1*0ubYRV_WNR9iYrzUAVINHw.jpeg",
+        scores: [
+            "4",
+            "4",
+            "5",
+            "1",
+            "5",
+            "2", 
+            "5",
+            "1",
+            "3",
+            "1", 
+        ]
+    },
+    {
+        name: "Natalia Romanova",
+        photo: "https://www.themarysue.com/wp-content/uploads/2014/08/the-avengers-black-widow-scarlett-johansson-wallpapers-480x800-06.jpg",
+        scores: [
+            "4",
+            "3",
+            "1",
+            "1",
+            "4",
+            "3",
+            "4",
+            "2",
+            "4",
+            "2",
+        ]
+    },
+    {
+        name: "Peter Quill",
+        photo: "https://www.newamericanjackets.com/wp-content/uploads/2017/03/Chris-Pratt-Guardians-of-the-Galaxy-2-Star-Lord-Coat-600x750.jpg",
+        scores: [
+            "5",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+        ]
+    },
+    {
+        name: "Jean Gray",
+        photo: "https://www.topcelebsjackets.com/wp-content/uploads/2015/11/Famke-Janssen-X-Men-Jean-Grey-Jacket-14-570x708.jpg",
+        scores: [
+            "3",
+            "4",
+            "4",
+            "2",
+            "3",
+            "3",
+            "1",
+            "1",
+            "3",
+            "3",
+        ] 
+    },
+    {
+        name: "Steven Rogers",
+        photo: "https://static1.squarespace.com/static/51b3dc8ee4b051b96ceb10de/t/55438ee2e4b08092fe70c735/1430490855043/the-science-behind-captain-americas-super-soldier-serum?format=2500w",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "2",
+            "2",
+            "2",
+            "3",
+            "5",
+            "1",
+            "4",
+        ]
+    }
+];
