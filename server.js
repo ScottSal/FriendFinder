f33e037791a30dbe19235e3a70949ea3b3b3b634
Scott Saloom <scottsaloom@gmail.com>
2018-09-07 13:43:31 -0500

    Uploaded code

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-var bodyParser = require("body-parser");
+// ===========================================================================
+// Dependencies
+// ===========================================================================
+
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
-
+//Express Server
 var app = express();
+//Port setting for local testing and server
 var PORT = process.env.PORT || 8080;
-
+//Static assets
 app.use(express.static(path.join(__dirname, "app/public")));
+//Implement bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use(bodyParser.json())
-
+// Routes
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
+// Server Listening 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+	console.log("Server listening on: http://localhost:" + PORT);
 });
