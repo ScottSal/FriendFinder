de22d7d2451a300f993bd83acd06a53cb7184a6f
Scott Saloom <scottsaloom@gmail.com>
2018-09-07 18:40:35 -0500

    Updated code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,46 +1,61 @@
-var path = require("path");
-var friends = require("../data/friends.js");
+// LOAD DATA
+// These data sources hold arrays of information on friends-data.
+// ===============================================================================
+var friends = require("../data/friends");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
-        res.json(friends);
-    });
+// ROUTING
+module.exports = function(app){
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
 
-    app.post("/api/friends", function(req, res) {
-        //determine who has closest answers to user for best match. 
-        //create object to measure difference between answers
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: 1000
-        };
+app.get("/api/friends", function(req,res){
+    res.json(friends)
+});
 
-        //create user data coming from user, take scores from data, assign var
-        var userData = req.body;
-        var userScores = userData.scores;
+app.post("/api/friends",function(req,res){
 
-        //calc difference between each user:
-        var totalDifference = 0;
-        //loop through friends in db
-        for (var i = 0; i < friends.length; i++) {
-            console.log(friends[i]);
-            totalDifference = 0
-            //loop through scores of each
-            for (var j = 0; j < friends[i].scores[j]; j++) {
-                //subtract user score from friends score, sum is totalDifference
-                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-                
-                if (totalDifference <=  bestMatch.friendDifference) {
-                    //resets bestMatch
-                    bestMatch.name = friends[i].name;
-                    bestMatch.photo = friends[i].photo;
-                    bestMatch.friendDifference = totalDifference;
-                } 
+// get the user input from survey form
+    var possibleFriend= req.body;
+
+    var scoreDifference=0;
+    var scoreDifferenceLow=40;
+    var scoreDifferenceLowIndex=0;
+    
+    for(i=0;i<possibleFriend.score;i++){
+        // disregard the string and get the number 1, if score is strongly disagree
+        if(possibleFriend.score[i]=="1 (Strongly Disagree"){
+            possibleFriend.score[i]==1;
+        }
+        // disregard the string and get the number 5, if score is strongly Agree
+        else if(possibleFriend.score[i]=="5 (Strongly Agree"){
+            possibleFriend.score[i]==5;
+        }
+        // keep the corresponding score value for score 2,3,4,5 
+        else{
+            possibleFriend.score[i]== parseInt(possibleFriend.score[i]);
+        }
+    }   
+        // compare the possibleFriend score with existing friends score on file.
+        for(i=0;i<friends.length;i++){
+            scoreDifference=0;//reset score difference when you start comparing to the next friend.
+            for(j=0;j<friends[i].score.length;j++){
+                //get absolute value of each score index difference 
+                scoreDifference += Math.abs((friends[i].score[j]-possibleFriend.score[j]));
+            }
+            if(scoreDifference <= scoreDifferenceLow){// if score difference is lower than the predifined lowscore difference
+             scoreDifferenceLow = scoreDifference;//replace scoredifference low with the new low score
+             scoreDifferenceLowIndex=i;// get that lowest score index which is the friend match index.
+  
             }
         }
-        //save user's data to db  
-        friends.push(userData);
+
+        var bestMatch={};
+        bestMatch = friends[scoreDifferenceLowIndex];
         res.json(bestMatch);
-    });
-    
+        friends.push(req.body)
+
+
+});
 };
